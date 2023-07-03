f696ebf3102d6159dad4ff4c5f392879f2fef56d
Tristan <tristanskyhook@gmail.com>
2023-07-03 15:02:30 -0700

    dz

--- src/page_components/sand/sand.rs
+++ src/page_components/sand/sand.rs
@@ -1,4 +1,5 @@
 
+use web_sys::WebGlBuffer;
 use web_sys::console::info;
 use yew::prelude::*;
 //use yew::virtual_dom::ListenerKind::onclick;
@@ -11,84 +12,7 @@ use web_sys::{window, HtmlCanvasElement, WebGl2RenderingContext as GL, WebGl2Ren
 
 use log::info;
 //use yew::{html, Component, Context, Html, NodeRef};
-
-pub struct Particle {
-    pub x: i32,
-    pub y: i32,
-    pub vx: i32,
-    pub vy: i32,
-    pub color: String,
-}
-
-pub enum ClicType {
-    Sand,
-    Water,
-    Wall,
-}
-
-pub struct Click {
-    pub x: i32,
-    pub y: i32,
-    pub clic_type: ClicType,
-}
-
-pub enum Msg {
-    //this is the message that is sent when the user clicks on the canvas
-    Click(Click),
-}
-
-pub struct SandGame {
-    pub width: i32,
-    pub height: i32,
-    pub particles: Vec<Particle>,
-    pub clicks: Vec<Click>,
-}
-
-impl SandGame {
-    pub fn update(&mut self) {
-        //update the particles
-        for particle in self.particles.iter_mut() {
-            particle.x += particle.vx;
-            particle.y += particle.vy;
-            particle.vy += 1;
-        }
-        //update the clicks
-        for click in self.clicks.iter_mut() {
-            match click.clic_type {
-                ClicType::Sand => {
-                    self.particles.push(Particle {
-                        x: click.x,
-                        y: click.y,
-                        vx: 0,
-                        vy: 0,
-                        color: String::from("yellow"),
-                    });
-                }
-                ClicType::Water => {
-                    self.particles.push(Particle {
-                        x: click.x,
-                        y: click.y,
-                        vx: 0,
-                        vy: 0,
-                        color: String::from("blue"),
-                    });
-                }
-                ClicType::Wall => {
-                    self.particles.push(Particle {
-                        x: click.x,
-                        y: click.y,
-                        vx: 0,
-                        vy: 0,
-                        color: String::from("black"),
-                    });
-                }
-            }
-        }
-        //clear the clicks
-        self.clicks.clear();
-    }
-}
-
+use crate::page_components::sand::sandgame::*;
 
 // Wrap gl in Rc (Arc for multi-threaded) so it can be injected into the render-loop closure.
 pub struct SandWindow {
@@ -98,6 +22,9 @@ pub struct SandWindow {
     //shared ref to the sandgame, which stores the states of the particles
     game: Rc<RefCell<SandGame>>,
     refrence_time: Rc<RefCell<f64>>,
+
+    boundry_buffer: WebGlBuffer,
+    particle_buffer: WebGlBuffer,
 }
 
 impl Component for SandWindow {
